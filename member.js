4f12b3647976c2d5362d93b0b03754dc4fb43dc8
tc33gibson <19958322+tc33gibson@users.noreply.github.com>
2023-07-18 14:54:49 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,11 @@
-function skillsmember() {
-    var skills = document.getElementById("skills");
-    var member = document.getElementById("member");
-    var skillsmember = document.getElementById("skillsmember");
-    var membermember = document.getElementById("membermember");
-    var skillsmembermember = document.getElementById("skillsmembermember");
-    skills.style.display = "none";
-    member.style.display = "none";
-    skillsmember.style.display = "block";
-    membermember.style.display = "none";
-    skillsmembermember.style.display = "none";
+function skillsMember() {    
+    var skills = document.getElementById("skills").value;
+    var skillsArray = skills.split(",");
+    var skillsList = document.getElementById("skillsList");
+    skillsList.innerHTML = "";
+    for (var i = 0; i < skillsArray.length; i++) {
+        var li = document.createElement("li");
+        li.innerHTML = skillsArray[i];
+        skillsList.appendChild(li);
+    }
 }
