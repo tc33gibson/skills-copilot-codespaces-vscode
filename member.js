c6e4c2956e98f103db16ff5dcee081dce4a9d75c
tc33gibson <19958322+tc33gibson@users.noreply.github.com>
2023-07-18 14:30:55 +0000

    Copilot second2 commit

--- member.js
+++ member.js
@@ -1,22 +1,6 @@
 function skillsmember() {
-    var member = document.getElementById("member");
     var skills = document.getElementById("skills");
-    var project = document.getElementById("project");
-    var contact = document.getElementById("contact");
-    var about = document.getElementById("about");
-    var skill = document.getElementById("skill");
-    var mem = document.getElementById("mem");
-    var pro = document.getElementById("pro");
-    var con = document.getElementById("con");
-    var abo = document.getElementById("abo");
-    member.style.display = "block";
-    skills.style.display = "none";
-    project.style.display = "none";
-    contact.style.display = "none";
-    about.style.display = "none";
-    skill.style.backgroundColor = "white";
-    mem.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
-    pro.style.backgroundColor = "white";
-    con.style.backgroundColor = "white";
-    abo.style.backgroundColor = "white";
+    var member = document.getElementById("member");
+    var skillsmember = document.getElementById("skillsmember");
+    var member = document.getElementById("member");
 }
