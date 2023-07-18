a91d1432d9c9f2eec2ddfcf95f0b62583217188b
tc33gibson <19958322+tc33gibson@users.noreply.github.com>
2023-07-18 14:49:22 +0000

    Copilot second3 commit

--- member.js
+++ member.js
@@ -1,6 +1,1 @@
 function skillsmember() {
-    var skills = document.getElementById("skills");
-    var member = document.getElementById("member");
-    var skillsmember = document.getElementById("skillsmember");
-    var member = document.getElementById("member");
-}
